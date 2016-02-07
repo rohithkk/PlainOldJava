b5fe4e0f25c5c100e6da1af3aad4a629d9968fbb
rohithkk <rohithkodakandla@gmail.com>
2016-02-06 22:56:55 -0500

    first time commmit of PlainJavaProject

---
+++ PlainJavaProject/src/com/rohith/PlainOldClass.java
@@ -0,0 +1,12 @@
+package com.rohith;
+
+public class PlainOldClass {
+
+	public PlainOldClass() {
+		super();
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Say Hello to my little friend");
+	}
+}
